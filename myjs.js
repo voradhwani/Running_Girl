4eab451dda2aa166a7c63423c807936486ce5072
Dhwani Vora <64498043+voradhwani@users.noreply.github.com>
2021-05-24 01:21:01 +0530

    feature: btn-disable on func run and add audio

--- myjs.js
+++ myjs.js
@@ -1,10 +1,18 @@
 m = 0;
 var y;
+var audio = document.getElementById("audio");
 
 function start() {
     y = setInterval(run, 50);
 
     function run() {
+        // add music 
+        audio.play();
+
+        // btn-disable 
+        document.getElementById("start-btn").disabled = true;
+        document.getElementById("stop-btn").disabled = false;
+
 
         if (m == 1200) {
             clearInterval(y);
@@ -12,12 +20,16 @@ function start() {
 
         } else {
             m += 5;
-            var x = document.getElementById("img");
-            x.style.marginLeft = m + "px";
+            var img = document.getElementById("img");
+            img.style.marginLeft = m + "px";
         }
     }
 }
 
 function stop() {
     clearInterval(y);
+    audio.pause();
+    document.getElementById("stop-btn").disabled = true;
+    document.getElementById("start-btn").disabled = false;
+
 }
