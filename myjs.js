137dba84afd3e543c851778b0b114479a0da2776
Dhwani Vora <64498043+voradhwani@users.noreply.github.com>
2021-05-25 01:47:25 +0530

    feature: right to left

--- myjs.js
+++ myjs.js
@@ -1,55 +1,70 @@
 m = 0;
 let y;
 let audio = document.getElementById("audio");
+let img = document.getElementById("img");
+let x;
 
-function start() {
-  console.log('start called');
-  y = setInterval(run, 150);
-  
-  function run() {
+function run() {
     // add music 
     audio.play();
-    
+
     // btn-disable 
     document.getElementById("start-btn").disabled = true;
     document.getElementById("stop-btn").disabled = false;
-    
-    
+
+
+
+    console.log(m);
     if (m == 1200) {
-      clearInterval(y);
-      m = 0;
-      stop();
-      flipCharacter();
+        stop();
+        flipCharacter();
+        m -= 50;
+
     } else {
-      m += 5;
-      let img = document.getElementById("img");
-      img.style.marginLeft = m + "px";
+        if (m >= 0) {
+            x = img.classList;
+            if (x.contains("flipped-character")) {
+                m -= 50;
+                img.style.marginLeft = m + "px";
+            } else {
+                m += 50;
+                img.style.marginLeft = m + "px";
+            }
+        }
+        else{
+            stop();
+        }
     }
-  }
+}
+
+function start() {
+    console.log('start called');
+    y = setInterval(run, 150);
+
+
 }
 
 function stop() {
-  y = clearInterval(y);
-  audio.pause();
-  document.getElementById("stop-btn").disabled = true;
-  document.getElementById("start-btn").disabled = false;
+    y = clearInterval(y);
+    audio.pause();
+    document.getElementById("stop-btn").disabled = true;
+    document.getElementById("start-btn").disabled = false;
 }
 
-function flipCharacter(){
-  let img = document.getElementById("img");
-  img.classList.add('flipped-character');
+function flipCharacter() {
+    let img = document.getElementById("img");
+    img.classList.add('flipped-character');
 }
 
 document.onkeyup = function (e) {
-  switch (e.key) {
-    case " " :
-    // running => stop 
-    if(y){
-      stop();
+    switch (e.key) {
+        case " ":
+            // running => stop 
+            if (y) {
+                stop();
+            } else {
+                start();
+            }
+            break;
     }
-    else{
-      start();
-    }
-    break;
-  }
 }
