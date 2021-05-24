68ff9ddd4e0cf058434e2070835be2ef77183a9d
Abhishek Jain <ajj.2396@gmail.com>
2021-05-25 00:35:37 +0530

    Added Logic to flipped-character

--- myjs.js
+++ myjs.js
@@ -3,47 +3,53 @@ let y;
 let audio = document.getElementById("audio");
 
 function start() {
-    y = setInterval(run, 50);
-
-    function run() {
-        // add music 
-        audio.play();
-
-        // btn-disable 
-        document.getElementById("start-btn").disabled = true;
-        document.getElementById("stop-btn").disabled = false;
-
-
-        if (m == 1200) {
-            clearInterval(y);
-            m = 0;
-
-        } else {
-            m += 5;
-            let img = document.getElementById("img");
-            img.style.marginLeft = m + "px";
-        }
+  console.log('start called');
+  y = setInterval(run, 150);
+  
+  function run() {
+    // add music 
+    audio.play();
+    
+    // btn-disable 
+    document.getElementById("start-btn").disabled = true;
+    document.getElementById("stop-btn").disabled = false;
+    
+    
+    if (m == 1200) {
+      clearInterval(y);
+      m = 0;
+      stop();
+      flipCharacter();
+    } else {
+      m += 5;
+      let img = document.getElementById("img");
+      img.style.marginLeft = m + "px";
     }
+  }
 }
 
 function stop() {
-    y = clearInterval(y);
-    audio.pause();
-    document.getElementById("stop-btn").disabled = true;
-    document.getElementById("start-btn").disabled = false;
+  y = clearInterval(y);
+  audio.pause();
+  document.getElementById("stop-btn").disabled = true;
+  document.getElementById("start-btn").disabled = false;
+}
 
+function flipCharacter(){
+  let img = document.getElementById("img");
+  img.classList.add('flipped-character');
 }
 
 document.onkeyup = function (e) {
-    switch (e.key) {
-        case " " :
-            // running => stop 
-            if(y){
-                stop();
-            }
-            else{
-                start();
-            }
-            break;
+  switch (e.key) {
+    case " " :
+    // running => stop 
+    if(y){
+      stop();
     }
+    else{
+      start();
+    }
+    break;
   }
+}
